8786ef52aae950c2a5f8714288e425e080d9a4bd
Helen23781 <cardenaselena247@gmail.com>
2025-01-30 23:34:33 -0500

    eliminacion de piso

--- controller/pisoController.js
+++ controller/pisoController.js
@@ -1,4 +1,6 @@
 const Piso = require("../models/pisos");
+const Torre = require("../models/torres");
+const AppError = require("../error/AppError");
 
 const getPiso = async () => {
   try {
@@ -11,7 +13,7 @@ const getPiso = async () => {
 
 const createPiso = async (numero_piso, jefe_piso, becaId) => {
   try {
-    const piso = await Piso.create({ numero_piso, jefe_piso,becaId});
+    const piso = await Piso.create({ numero_piso, jefe_piso, becaId });
     return piso;
   } catch (error) {
     throw error;
@@ -21,7 +23,7 @@ const createPiso = async (numero_piso, jefe_piso, becaId) => {
 const updatePiso = async (id, numero_piso, jefe_piso, becaId) => {
   try {
     const piso = await Piso.update(
-      { numero_piso, jefe_piso,becaId },
+      { numero_piso, jefe_piso, becaId },
       { where: { id } }
     );
     return piso;
@@ -31,14 +33,20 @@ const updatePiso = async (id, numero_piso, jefe_piso, becaId) => {
 };
 
 const deletePiso = async (id) => {
-    try {
-      const piso = await Piso.destroy(
-        { where: { id } }
+  try {
+    // Verificar si hay torres asociadas al piso
+    const torresAsociadas = await Torre.count({ where: { pisoId: id } });
+    if (torresAsociadas > 0) {
+      throw new AppError(
+        "No se puede eliminar el piso cuando tiene torres asociadas.",
+        400
       );
-      return piso;
-    } catch (error) {
-      throw error;
     }
-  };
-module.exports = { createPiso ,updatePiso, getPiso, deletePiso};
-   
+    const piso = await Piso.destroy({ where: { id } });
+    return piso;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { createPiso, updatePiso, getPiso, deletePiso };
